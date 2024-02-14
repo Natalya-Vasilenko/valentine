c6c73da94d73f5e021a862d693111ed1c779efde
Natalya Vasilenko <Natalli011@mail.ru>
2024-02-14 16:39:31 +0300

    'Olezhek'

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,22 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "Нет",
+      "Вы уверены?",
+      "Точно?",
+      "Подумойте..",
+      "Еще подумойте!",
+      "Точно нет?",
+      "Пожалеете, пожалеете!",
+      "Это может быть ошибка всей вашей жизни...",
+      "Имейте совесть!",
+      "Ради Христа!!",
+      "Вы что, неверующий?",
+      "А за липтон?",
+      "А за кока-колку?",
+      "Вы разбиваете мне сердечко(",
+      "Это ваш окончательный ответ?",
+      "Плиииииз, вы разбиваете мне сердечко((",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -43,7 +40,7 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            Ура! Победа победа!!!
           </div>
         </>
       ) : (
@@ -61,7 +58,7 @@ export default function Page() {
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Олежек, пойдемте на свидание?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -69,13 +66,13 @@ export default function Page() {
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Да
             </button>
             <button
               onClick={handleNoClick}
               className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Нет" : getNoButtonText()}
             </button>
           </div>
         </>
